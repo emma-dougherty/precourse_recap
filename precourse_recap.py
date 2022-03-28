abbef35c4c225dc229e4de5ad11bf458ab5872e8
emma-dougherty <doughertyemma@hotmail.com>
2022-03-28 16:42:45 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+new_student = "Emma"
+cohort = 57
+congrats_msg = "Congratulations" +" " new_student + " " + "on completing your first day!"
+
+print(congrats_msg)
+
+cake_portions = 27
+total_class_size = 25
+cake_left_over = cake_portions - total_class_size
+
+print (cake_left_over)
